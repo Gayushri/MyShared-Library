9e18869430908f9b3bd8d44ed19e432c88e64ca4
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-02-07 20:31:07 +0530

    Create artifact.groovy

---
+++ vars/artifact.groovy
@@ -0,0 +1,18 @@
+def artifact()
+script {    
+       pom = readMavenPom file: 'pom.xml'
+
+         nexusArtifactUploader artifacts: [
+         [artifactId: 'maven-project', 
+         classifier: '', 
+         file: '/var/lib/jenkins/workspace/shared-job/webapp/target/webapp.war', 
+         type: 'war']
+          ],
+          credentialsId: 'nexus', 
+          groupId: 'com.example.maven-project', 
+          nexusUrl: '15.206.81.221:8081', 
+          nexusVersion: 'nexus3', 
+          protocol: 'http', 
+          repository: 'java_job', 
+          version: "${pom.version}"
+        }
