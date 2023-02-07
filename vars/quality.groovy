af2a6594b29b994797f0a54dc64508df11281060
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-02-07 18:58:06 +0530

    Create quality.groovy

---
+++ vars/quality.groovy
@@ -0,0 +1,12 @@
+def quality_gate() {
+  timeout(time: 5, unit: MINUTES') {
+  echo "Initializing quality gates.."
+  def result = waitForQualityGate()
+  if (result.status != 'OK') {
+    error "Pipeline aborted: ${result.status}"
+  }
+  else {
+    echo "Quality passed: ${result.status}"
+  }
+ }         
+}
