50803b11ee5a38830249b858586b4ed50ec0b603
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-02-08 12:20:12 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,4 +1,4 @@
-def deploy() {
+def war () {
   sshagent(['deploy']) {
    sh "scp -o StrictHostKeyChecking=no webapp/target/webapp.war ubuntu@13.233.59.113:/opt/tomcat/apache-tomcat-9.0.71/webapps"
    }
